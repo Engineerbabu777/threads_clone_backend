2d2bcba5d36060a73620067ef594a8977a8a67c3
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-28 21:04:08 +0500

    Add: code splitting

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,17 @@
-import express from 'express'
-import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server/express4'
-import { prisma } from './lib/db'
+import { expressMiddleware } from "@apollo/server/express4";
+import createApolloGraphqlServer from "./graphql";
+import express from "express";
+async function init() {
+  const app = express();
+  const PORT = Number(process.env.PORT) || 8000;
 
-async function init () {
-  const app = express()
-  app.use(express.json())
+  app.use(express.json());
 
-  const gqlServer = new ApolloServer({
-    typeDefs: `
-     type Query{
-        hello: String
-        say(name:String): String
-     }
+  app.get("/", (req, res) => {
+    res.json({ message: "Server is up and running" });
+  });
 
-     type Mutation{
-      createUser(email:String!,password:String,firstName:String!,lastName:String!):Boolean
-     }
-    `, // Schema!
-    resolvers: {
-      Query: {
-        hello: () => 'Hello',
-        say: (_, { name }) => name + '5'
-      },
-      Mutation: {
-        createUser: async (
-          _,
-          {
-            firstName,
-            lastName,
-            email,
-            password
-          }: {
-            firstName: string
-            lastName: string
-            email: string
-            password: string
-          }
-        ) => {
-          await prisma.user.create({
-            data: {
-              email,
-              password,
-              firstName,
-              lastName,
-              salt: 'uueu'
-            }
-          })
-          return true
-        }
-      }
-    }
-  })
+  app.use("/graphql", expressMiddleware(await createApolloGraphqlServer()));
 
-  await gqlServer.start()
-
-  app.get('/', async (req: any, res: any) => {
-    res.send('Hello world!')
-  })
-
-  app.use('/graphql', expressMiddleware(gqlServer))
-
-  app.listen(8000, () => {
-    console.log('Server is Running...')
-  })
+  app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`));
 }
-
-init()
